0ced0c27bd58e85dd1e3698302f008fa266c5d9c
Анатолий Курилов < >
2017-02-23 16:03:19 +0300

    'norma'

---
+++ lecture01/src/main/java/ru.atom/HelloWorld.java
@@ -0,0 +1,12 @@
+package ru.atom;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(getHelloWorld());
+    }
+
+    public static String getHelloWorld() {
+        return "Hello, World!";
+    }
+}
